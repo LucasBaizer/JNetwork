46fb3f93a09a2b449f40a44028b608fb000f1ca5
TitaniumSapphire <titaniumsapphire32@gmail.com>
2016-01-21 19:17:53 -0800

    Uploaded examples of how to use JNetwork's object I/O
    Includes an example server and client class for transferring objects.

---
+++ example/ObjectTransferClient.java
@@ -0,0 +1,62 @@
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import org.jnetwork.AdvancedInputStream;
+import org.jnetwork.AdvancedOutputStream;
+import org.jnetwork.CloseRequest;
+import org.jnetwork.Connection;
+import org.jnetwork.DataPackage;
+
+/**
+ * This class reads an object in from a server and then sends an object back.
+ */
+public class ObjectTransferClient {
+	public static void main(String[] args) {
+		try {
+			// connects to the server
+			Connection client = new Connection("thehostaddress", 1337);
+
+			// the client socket will be closed on program exit
+			CloseRequest.addObjectToClose(client);
+
+			// gets the output stream of the server, which data will be written
+			// out to
+			AdvancedOutputStream out = client.getOutputStream();
+
+			// gets the input stream of the server, which data will be read from
+			AdvancedInputStream in = client.getInputStream();
+
+			// reads the first DataPackage sent my the server
+			DataPackage packageFromServer = (DataPackage) in.readSpecificType(DataPackage.class);
+
+			// prints out the received package's message, followed by the first
+			// and second
+			// pieces of data added to the package by the server
+			System.out.println(packageFromServer.getMessage() + ": " + packageFromServer.getObjects()[0]
+					+ " Your IP Address is " + packageFromServer.getObjects()[1]);
+
+			// creates a new DataPackage containing a string saying "Hello,
+			// Server." and a message
+			// saying "ImportantPackageFromClient"
+			DataPackage pkgOut = new DataPackage("Hello, server!").setMessage("ImportantPackageFromClient");
+			out.writeObject(pkgOut);
+
+			// closes the client and all of its streams
+			client.close();
+		} catch (UnknownHostException e) {
+			System.err.println("The host doesn't exist!");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.err.println("An I/O error occured while connecting to the server.");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.err.println("The server sent an object who's type does not exist on this machine.");
+			e.printStackTrace();
+		}
+
+		/**
+		 * OUTPUT: ImportantPackageFromServer: Hello, client! Your IP Address is
+		 * [the IP address].
+		 */
+	}
+}
---
+++ example/ObjectTransferServer.java
@@ -0,0 +1,79 @@
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.SocketAddress;
+
+import org.jnetwork.AdvancedInputStream;
+import org.jnetwork.AdvancedOutputStream;
+import org.jnetwork.CloseRequest;
+import org.jnetwork.DataPackage;
+import org.jnetwork.Server;
+import org.jnetwork.SocketPackage;
+import org.jnetwork.event.ClientConnectionListener;
+
+/**
+ * This class sends an object to each client that connects and then reads an
+ * object in from the client.
+ */
+public class ObjectTransferServer implements ClientConnectionListener {
+	private static final long serialVersionUID = 1464765717434969912L;
+
+	private static Server server;
+
+	public static void main(String[] args) throws IOException {
+		// creates a new server on port 1337 with the ClientConnectionListener
+		// as an instance of this class
+		server = new Server(1337, new ObjectTransferServer());
+
+		// the server socket will be closed on program exit
+		CloseRequest.addObjectToClose(server);
+	}
+
+	// this method is called each time a client connects
+	@Override
+	public void clientConnected(SocketPackage event) {
+		// gets the IP address of the client
+		SocketAddress address = event.getSocket().getRemoteSocketAddress();
+
+		// gets the output stream of the client, which data will be written out
+		// to
+		AdvancedOutputStream out = event.getOutputStream();
+
+		// gets the input stream of the client, which data will be read from
+		AdvancedInputStream in = event.getInputStream();
+
+		// creates a new DataPackage containing a string saying "Hello, client."
+		// and the client's IP address. it also gives the package a message
+		// saying "ImportantPackageFromServer"
+		DataPackage pkg = new DataPackage("Hello, client!", address).setMessage("ImportantPackageFromServer");
+
+		try {
+			// writes the package out to the client
+			out.writeObject(pkg);
+		} catch (IOException e) {
+			System.err.println("There was an error sending the object to the client.");
+			e.printStackTrace();
+		}
+
+		try {
+			// reads the first DataPackage sent by the client
+			DataPackage packageFromClient = (DataPackage) in.readSpecificType(DataPackage.class);
+
+			// prints out the received package's message, followed by the first
+			// piece of data added to the package by the client
+			System.out.println(packageFromClient.getMessage() + ": " + packageFromClient.getObjects()[0]);
+		} catch (IOException e) {
+			System.err.println("There was an I/O error reading the package.");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.err.println("The client sent an object who's type does not exist on this machine.");
+			e.printStackTrace();
+		}
+
+		// the method exits, all streams are closed
+		// automatically, along with the client socket
+
+		/**
+		 * OUTPUT: ImportantPackageFromClient: Hello, server!
+		 */
+	}
+}
